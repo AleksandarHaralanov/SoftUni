9b9b10af8e042c6f5b5f817f72609fac4d40e5e0
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-09-07 23:59:00 +0300

    Add new exercise

---
+++ src/io/github/aleksandarharalanov/softuni/java/basics/firststepsincoding/exercise/code/SuppliesForSchool.java
@@ -0,0 +1,19 @@
+package io.github.aleksandarharalanov.softuni.java.basics.firststepsincoding.exercise.code;
+
+import java.util.Scanner;
+
+public class SuppliesForSchool {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int pens = Integer.parseInt(scanner.nextLine());
+            int markers = Integer.parseInt(scanner.nextLine());
+            int liters = Integer.parseInt(scanner.nextLine());
+            int discount = Integer.parseInt(scanner.nextLine());
+            double sum = (pens * 5.8 + markers * 7.2 + liters * 1.2);
+            double finalSum = sum - (sum * ((double) discount / 100));
+            System.out.println(finalSum);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid number (integer).");
+        }
+    }
+}
