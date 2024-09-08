406d4ce0ef18fe76caced6db6f5978842c2e5606
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-09-08 17:29:05 +0300

    Add conditional exercise

---
+++ src/io/github/aleksandarharalanov/softuni/java/basics/conditionalstatements/exercise/code/SumSeconds.java
@@ -0,0 +1,25 @@
+package io.github.aleksandarharalanov.softuni.java.basics.conditionalstatements.exercise.code;
+
+import java.util.Scanner;
+
+public class SumSeconds {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int timeFirst = Integer.parseInt(scanner.nextLine());
+            int timeSecond = Integer.parseInt(scanner.nextLine());
+            int timeThird = Integer.parseInt(scanner.nextLine());
+
+            int totalTime = timeFirst + timeSecond + timeThird;
+            int minutes = totalTime / 60;
+            int seconds = totalTime % 60;
+
+            if (seconds < 10) {
+                System.out.printf("%d:0%d", minutes, seconds);
+            } else {
+                System.out.printf("%d:%d", minutes, seconds);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid integer.");
+        }
+    }
+}
