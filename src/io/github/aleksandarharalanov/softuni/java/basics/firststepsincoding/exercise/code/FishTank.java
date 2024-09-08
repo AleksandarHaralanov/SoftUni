904158452c8ecd8166fb5a7d739845b4822ed545
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-09-08 12:24:01 +0300

    Add new exercise

---
+++ src/io/github/aleksandarharalanov/softuni/java/basics/firststepsincoding/exercise/code/FishTank.java
@@ -0,0 +1,22 @@
+package io.github.aleksandarharalanov.softuni.java.basics.firststepsincoding.exercise.code;
+
+import java.util.Scanner;
+
+public class FishTank {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int length = Integer.parseInt(scanner.nextLine());
+            int width = Integer.parseInt(scanner.nextLine());
+            int height = Integer.parseInt(scanner.nextLine());
+            double percent = Double.parseDouble(scanner.nextLine());
+
+            int aquariumVolume = length * width * height;
+            double litersVolume = aquariumVolume * 0.001;
+            double litersNeeded = litersVolume * (1 - (percent / 100));
+
+            System.out.println(litersNeeded);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid number (integer or double).");
+        }
+    }
+}
