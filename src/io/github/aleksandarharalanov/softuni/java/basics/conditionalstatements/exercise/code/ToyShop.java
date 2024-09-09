6c997c57fc059ed9ea466a18d042d552044ce3eb
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-09-09 18:20:35 +0300

    Add ToyShop exercise

---
+++ src/io/github/aleksandarharalanov/softuni/java/basics/conditionalstatements/exercise/code/ToyShop.java
@@ -0,0 +1,32 @@
+package io.github.aleksandarharalanov.softuni.java.basics.conditionalstatements.exercise.code;
+
+import java.util.Scanner;
+
+public class ToyShop {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            double price = Double.parseDouble(scanner.nextLine());
+            int puzzles = Integer.parseInt(scanner.nextLine());
+            int dolls = Integer.parseInt(scanner.nextLine());
+            int plushies = Integer.parseInt(scanner.nextLine());
+            int minions = Integer.parseInt(scanner.nextLine());
+            int trucks = Integer.parseInt(scanner.nextLine());
+
+            int count = puzzles + dolls + plushies + minions + trucks;
+            double earnings = puzzles * 2.6 + dolls * 3 + plushies * 4.1 + minions * 8.2 + trucks * 2;
+
+            if (count >= 50) {
+                earnings = earnings * 0.75;
+            }
+            earnings = earnings * 0.9;
+
+            if (earnings > price) {
+                System.out.printf("Yes! %.2f lv left.", earnings - price);
+            } else {
+                System.out.printf("Not enough money! %.2f lv needed.", Math.abs(earnings - price));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid number (integer or double).");
+        }
+    }
+}
