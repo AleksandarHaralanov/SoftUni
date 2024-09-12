51cd2c4051e53851b0dddf2e7d90db37a70918ac
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-09-12 18:27:39 +0300

    Simplified to meet exercise step standards, added explanation for an easier method with a for-loop in a comment

--- src/io/github/aleksandarharalanov/softuni/java/basics/firststepsincoding/lab/code/Nums1To10.java
+++ src/io/github/aleksandarharalanov/softuni/java/basics/firststepsincoding/lab/code/Nums1To10.java
@@ -2,8 +2,21 @@ package io.github.aleksandarharalanov.softuni.java.basics.firststepsincoding.lab
 
 public class Nums1To10 {
     public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+        System.out.println(6);
+        System.out.println(7);
+        System.out.println(8);
+        System.out.println(9);
+        System.out.println(10);
+
+        /* Easier done with a for-loop, but done like this for the sake of the exercise steps.
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
+        */
     }
 }
