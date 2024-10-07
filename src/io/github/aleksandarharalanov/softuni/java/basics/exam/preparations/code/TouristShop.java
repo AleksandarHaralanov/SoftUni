52b5fe92d6625ac9879829fe26ac491dd57e5186
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-10-07 23:40:08 +0300

    Add new exercise

---
+++ src/io/github/aleksandarharalanov/softuni/java/basics/exam/preparations/code/TouristShop.java
@@ -0,0 +1,40 @@
+package io.github.aleksandarharalanov.softuni.java.basics.exam.preparations.code;
+
+import java.util.Scanner;
+
+public class TouristShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double budget = Double.parseDouble(scanner.nextLine());
+
+        int count = 0;
+        double total = 0;
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("Stop")) {
+                break;
+            }
+
+            double price = Double.parseDouble(scanner.nextLine());
+            count++;
+
+            if (count % 3 == 0) {
+                price /= 2;
+            }
+
+            if (budget < price) {
+                double need = Math.abs(budget - price);
+                System.out.println("You don't have enough money!");
+                System.out.printf("You need %.2f leva!", need);
+                return;
+            }
+
+            total += price;
+            budget -= price;
+        }
+
+        System.out.printf("You bought %d products for %.2f leva.", count, total);
+    }
+}
