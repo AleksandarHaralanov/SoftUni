2929d6e30babd1e29cc279ac45f46908d98c800a
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-10-19 18:49:06 +0300

    Add new exercise

--- src/io/github/aleksandarharalanov/softuni/java/basics/exam/code/UniquePINCodes.java
+++ src/io/github/aleksandarharalanov/softuni/java/basics/exam/code/UniquePINCodes.java
@@ -6,10 +6,31 @@ public class UniquePINCodes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int i = Integer.parseInt(scanner.nextLine());
-        int j = Integer.parseInt(scanner.nextLine());
-        int k = Integer.parseInt(scanner.nextLine());
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int c = Integer.parseInt(scanner.nextLine());
 
+        for (int i = 2; i <= a; i += 2) {
+            for (int j = 2; j <= b; j++) {
+                if (isPrime(j)) {
+                    for (int k = 2; k <= c; k += 2) {
+                        System.out.printf("%d %d %d%n", i, j, k);
+                    }
+                }
+            }
+        }
+    }
 
+    private static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+
+        for (int k = 2; k * k <= num; k++) {
+            if (num % k == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
